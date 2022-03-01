6686b0c7fb94bb9fd8281a89d98d9e8e36b6b3ee
uttamd12 <100711449+uttamd12@users.noreply.github.com>
2022-03-01 22:44:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+let display = document.getElementById('display');
+let buttons = Array.from(document.getElementsByClassName('button'));
+
+buttons.map(button => {
+  button.addEventListener('click', (e) => {
+    switch (e.target.innerText) {
+      case 'c':
+        display.innerText=''; 
+        break;
+      case 'del'  :
+        if (display.innerText){
+          display.innerText =display.innerText.slice(0,-1);
+        }
+        break;
+        case '=':
+          try{
+            display.innerText=eval(display.innerText); 
+          } catch{
+          display.innerText='error' 
+          }
+      default:
+        display.innerText += e.target.innerText;
+    }
+  });
+});
